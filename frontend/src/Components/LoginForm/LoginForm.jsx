4bcbecf75c7be497e4f13fa8f8172de817f87f37
m.backi <backimichal@gmail.com>
2023-09-20 22:08:22 +0200

    bez ikon

---
+++ frontend/src/Components/LoginForm/LoginForm.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import styles from "./LoginForm.module.css";
+import wallet from "../../assets/mini-wallet.svg";
+import emailIcon from "../../assets/email-icon.svg";
+import lockIcon from "../../assets/lock-icon.svg";
+
+const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <div className={styles.login}>
+      <div className={styles.login__header}>
+        <img src={wallet} alt="Wallet" />
+        <h2>Wallet</h2>
+      </div>
+      <form className={styles.login__form} onSubmit={handleSubmit}>
+        <label>
+          <img src={emailIcon} alt="email" />
+          <input
+            type="email"
+            value={email}
+            placeholder="E-mail"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          <img src={lockIcon} alt="lock" />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            placeholder="Password"
+          />
+        </label>
+        <button className={styles.login__signin} type="submit">
+          LOG IN
+        </button>
+        <button className={styles.login__signup} type="submit">
+          REGISTER
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
---
+++ frontend/src/Pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import { useState } from "react";
+import LoginForm from "../../Components/LoginForm/LoginForm";
+import styles from "./LoginPage.module.css";
+import frametablet from "../../assets/icon-main-tablet.png";
+import framedesktop from "../../assets/icon-main.png";
+
+const LoginPage = () => {
+  return (
+    <div className={styles.login__container}>
+      <div className={styles.login__tablet}>
+        {window.innerWidth > 1200 ? (
+          <img src={framedesktop} alt="framedesktop" />
+        ) : (
+          <img src={frametablet} alt="frametablet" />
+        )}
+
+        <h3>Finance App</h3>
+        <div className={styles.firstelipse}></div>
+        <div className={styles.secondelipse}></div>
+      </div>
+      <div className={styles.login__desktop}>
+        <LoginForm />
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
