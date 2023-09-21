3611c01f644823da6875b4c7f1f69500a4395393
Mykola Kovalskyi <112631611+mykolakovalskyi@users.noreply.github.com>
2023-09-21 18:37:46 +0200

    dev merged to feature/redux

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,6 @@ import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
