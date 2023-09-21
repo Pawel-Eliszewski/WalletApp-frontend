17e2ee4d1547f2e32e50c52b5fa8e14033f83df8
Pawel-Eliszewski <eliszewskipawel@gmail.com>
2023-09-21 20:47:49 +0200

    All views done, redux still in progress

--- frontend/src/components/ModalAddTransaction/ModalAddTransaction.jsx
+++ frontend/src/components/ModalAddTransaction/ModalAddTransaction.jsx
@@ -1,8 +1,10 @@
 import PropTypes from "prop-types";
+import { Header } from "../Header/Header";
 import { Calendar } from "./Calendar/Calendar";
 import { useState } from "react";
 import { CustomizedMuiSwitch } from "./CustomizedMuiSwitch/CustomizedMuiSwitch";
 import { DropdownMenu } from "../DropdownMenu/DropdownMenu";
+import { Show } from "@chakra-ui/react";
 import css from "./ModalAddTransaction.module.css";
 
 export const ModalAddTransaction = ({ userName }) => {
@@ -31,46 +33,44 @@ export const ModalAddTransaction = ({ userName }) => {
 
   const incomeClass = isIncome ? css.income : "";
   const expenseClass = !isIncome ? css.expense : "";
-
-  userName = "Paweł";
+  const backdropClass = isModalAddTransactionOpen
+    ? css.backdropIsOpen
+    : css.backdrop;
 
   // jeśli category !== "null" odpal onSubmit
 
   return (
-    <div className={css.wrapper}>
-      <div className={css.header}>
-        <div className={css.logoWrapper}>
-          <img className={css.logo} src="/assets/icon-wallet.png"></img>
-          <img className={css.appName} src="/assets/icon-wallet-text.png"></img>
+    <div className={backdropClass}>
+      <div className={css.container}>
+        <Show breakpoint="(max-width: 767px)">
+          <Header userName={userName} />
+        </Show>
+        <h2 className={css.title}>Add transaction</h2>
+        <div className={css.switchContainer}>
+          <p className={incomeClass}>Income</p>
+          <CustomizedMuiSwitch onChange={handleIsIncome} />
+          <p className={expenseClass}>Expense</p>
         </div>
-        <div className={css.nav}>
-          <h3 className={css.userName}>{userName}</h3>
-          <img className={css.iconExit} src="/assets/icon-exit-doors.png"></img>
-        </div>
-      </div>
-      <h2 className={css.title}>Add transaction</h2>
-      <div className={css.switchContainer}>
-        <p className={incomeClass}>Income</p>
-        <CustomizedMuiSwitch onChange={handleIsIncome} />
-        <p className={expenseClass}>Expense</p>
-      </div>
-      <form className={css.form}>
-        {!isIncome ? (
-          <DropdownMenu
-            expenseCategory={expenseCategory}
-            onClick={handleExpenseCategory}
-          />
-        ) : null}
-        <input className={css.money} placeholder="0.00" required></input>
-        <Calendar date={date} onChange={handleNewDate} />
-        <textarea className={css.comment} placeholder="Comment"></textarea>
-        <button type="submit" className={css.btnGreen}>
-          ADD
+        <form className={css.form}>
+          {!isIncome ? (
+            <DropdownMenu
+              expenseCategory={expenseCategory}
+              onClick={handleExpenseCategory}
+            />
+          ) : null}
+          <div className={css.formInnerBox}>
+            <input className={css.money} placeholder="0.00" required></input>
+            <Calendar date={date} onChange={handleNewDate} />
+          </div>
+          <textarea className={css.comment} placeholder="Comment"></textarea>
+          <button type="submit" className={css.btnGreen}>
+            ADD
+          </button>
+        </form>
+        <button onClick={handleModalClose} className={css.btnCancel}>
+          CANCEL
         </button>
-      </form>
-      <button onClick={handleModalClose} className={css.btnCancel}>
-        CANCEL
-      </button>
+      </div>
     </div>
   );
 };
