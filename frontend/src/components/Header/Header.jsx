b719975ff2cc2fdd8eff8274f0720b6e5acc7873
SebastianZasadni <emblelele@gmail.com>
2023-09-25 20:11:07 +0200

    fix/header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -3,10 +3,11 @@ import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux/es/hooks/useSelector";
 import { selectIsModalLogoutOpen } from "../../redux/global/selectors";
 import { setIsModalLogoutOpen } from "../../redux/global/globalSlice";
+import { selectUser } from "../../redux/session/selectors";
 
 export const Header = () => {
   const dispatch = useDispatch();
-  const isModalLogoutOpen = useSelector(selectIsModalLogoutOpen);
+  // const isModalLogoutOpen = useSelector(selectIsModalLogoutOpen);
   const userName = useSelector(selectUser);
 
   const handleOpenModalLogout = () => {
@@ -15,23 +16,23 @@ export const Header = () => {
 
   return (
     <div className={css.wrapper}>
-      {isModalLogoutOpen ? (
+      {/* {isModalLogoutOpen ? (
         <ModalLogout />
-      ) : (
-        <div className={css.header}>
-          <div className={css.logoWrapper}>
-            <img className={css.logo} src="/assets/icon-wallet.png" />
-            <img className={css.appName} src="/assets/icon-wallet-text.png" />
-          </div>
-          <div className={css.nav}>
-            <p className={css.userName}>{userName}</p>
-            <div className={css.boxExit} onClick={handleOpenModalLogout}>
-              <img className={css.iconExit} src="/assets/icon-exit-doors.png" />
-              <p className={css.textExit}>Exit</p>
-            </div>
+      ) : ( */}
+      <div className={css.header}>
+        <div className={css.logoWrapper}>
+          <img className={css.logo} src="/assets/icon-wallet.png" />
+          <img className={css.appName} src="/assets/icon-wallet-text.png" />
+        </div>
+        <div className={css.nav}>
+          <p className={css.userName}>{userName}</p>
+          <div className={css.boxExit} onClick={handleOpenModalLogout}>
+            <img className={css.iconExit} src="/assets/icon-exit-doors.png" />
+            <p className={css.textExit}>Exit</p>
           </div>
         </div>
-      )}
+      </div>
+      {/* )} */}
     </div>
   );
 };
