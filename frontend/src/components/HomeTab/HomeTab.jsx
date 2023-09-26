f6db33bf53b5b5b8e86d337ba047653a444663c4
Guma96 <mateusz.kutarba96@gmail.com>
2023-09-26 15:37:55 +0200

    HomTab + Pagination

---
+++ frontend/src/components/HomeTab/HomeTab.jsx
@@ -0,0 +1,142 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useMedia } from "react-use";
+import { fixDate } from "helpers/fixDate";
+import { useGetTransactionsQuery } from "../../redux/transactionsSlice";
+import { PaginatedItems } from "../Pagination/Pagination";
+import { Balance } from "components/balance/balance";
+import { ButtonAddTransaction } from "../ButtonAddTransaction/ButtonAddTransaction";
+import styles from "./HomeTab.module.css";
+
+const HomeTab = () => {
+  const isMobile = useMedia("(max-width: 767px)");
+  const [itemOffset, setItemOffset] = useState(0);
+
+  const { data } = useGetTransactionsQuery(itemOffset);
+
+  if (!data) {
+    return null;
+  }
+
+  let { transactions, length } = data;
+  const itemsPerPage = 5;
+  const pageCount = Math.ceil(length / itemsPerPage);
+
+  return (
+    <div className={styles.homeWrapper}>
+      <Balance />
+      <table
+        className={styles.tableWrapper}
+        style={{ maxHeight: isMobile ? "60vh" : "auto", overflowY: "auto" }}
+      >
+        {!isMobile && (
+          <>
+            <thead>
+              <tr className={styles.tableHead}>
+                <th className={styles.tableHeadItem}>Date</th>
+                <th className={styles.tableHeadItem}>Type</th>
+                <th className={styles.tableHeadItem}>Category</th>
+                <th className={styles.tableHeadItem}>Comment</th>
+                <th className={styles.tableHeadItem}>Sum</th>
+                <th className={styles.tableHeadItem}>Balance</th>
+              </tr>
+            </thead>
+            <tbody>
+              {transactions.map(
+                ({ _id, date, type, category, comment, sum, balance }) => (
+                  <tr key={_id} className={styles.data}>
+                    <td className={styles.dataItem}>{fixDate(date)}</td>
+                    <td className={styles.dataItem}>
+                      {type === true ? "+" : "-"}
+                    </td>
+                    <td
+                      className={styles.dataItem}
+                      style={{ textAlign: "left" }}
+                    >
+                      {category}
+                    </td>
+                    <td
+                      className={styles.dataItem}
+                      style={{ textAlign: "left" }}
+                    >
+                      <span className={styles.dataComment}>{comment}</span>
+                    </td>
+                    <td
+                      className={styles.dataItem}
+                      style={{ textAlign: "right", fontWeight: "700" }}
+                    >
+                      <span
+                        className={styles.dataSum}
+                        data-type={type.toString()}
+                      >
+                        {sum}
+                      </span>
+                    </td>
+                    <td
+                      className={styles.dataItem}
+                      style={{ textAlign: "right" }}
+                    >
+                      {balance}
+                    </td>
+                  </tr>
+                )
+              )}
+            </tbody>
+          </>
+        )}
+      </table>
+
+      {isMobile && (
+        <>
+          {transactions.map(
+            ({ _id, date, type, category, comment, sum, balance }) => (
+              <ul
+                key={_id}
+                className={styles.dataMob}
+                data-type={type.toString()}
+              >
+                <li className={styles.dataItemMob}>
+                  <span className={styles.headItemMob}>Date</span>
+                  {fixDate(date)}
+                </li>
+                <li className={styles.dataItemMob}>
+                  <span className={styles.headItemMob}>Type</span>
+                  {type === true ? "+" : "-"}
+                </li>
+                <li className={styles.dataItemMob}>
+                  <span className={styles.headItemMob}>Category</span>
+                  {category}
+                </li>
+                <li className={styles.dataItemMob}>
+                  <span className={styles.headItemMob}>Comment</span>
+                  <span className={styles.dataComment}>{comment}</span>
+                </li>
+                <li
+                  className={styles.dataItemMob}
+                  style={{ fontWeight: "700" }}
+                >
+                  <span className={styles.headItemMob}>Sum</span>
+                  <span className={styles.dataSum} data-type={type.toString()}>
+                    {sum}
+                  </span>
+                </li>
+                <li className={styles.dataItemMob}>
+                  <span className={styles.headItemMob}>Balance</span>
+                  {balance}
+                </li>
+              </ul>
+            )
+          )}
+        </>
+      )}
+
+      {pageCount > 1 && (
+        <PaginatedItems pageCount={pageCount} setItemOffset={setItemOffset} />
+      )}
+
+      <ButtonAddTransaction />
+    </div>
+  );
+};
+
+export default HomeTab;
