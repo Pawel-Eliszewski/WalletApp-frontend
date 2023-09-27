d554b5ee03c1a7c25ade0b5cb5c28024e65509fd
Sylvia Stachera <113374423+SylviaStachera@users.noreply.github.com>
2023-09-27 15:47:23 +0200

    Added corrected component navigation

---
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -0,0 +1,28 @@
+import { NavLink } from "react-router-dom";
+import css from "./Navigation.module.css";
+
+export const Navigation = () => {
+	return (
+		<nav>
+			<ul className={css.nav}>
+				<li>
+					<NavLink className={css.item} to="#">
+						<img className={css.logo} src="/assets/icon-home.svg" />
+						<p className={css.description}>Home</p>
+					</NavLink>
+				</li>
+				<li>
+					<NavLink className={css.item} to="#">
+						<img className={css.logo} src="/assets/icon-statistics.png" />
+						<p className={css.description}>Statistics</p>
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to="#">
+						<img className={`${css.logo} ${css.dolar}`} src="/assets/icon-dolar.png" />
+					</NavLink>
+				</li>
+			</ul>
+		</nav>
+	);
+};
