1e3fcef377d5d696b11f55872527ce7988042627
Sylvia Stachera <113374423+SylviaStachera@users.noreply.github.com>
2023-09-29 18:27:06 +0200

    Change navigation icon style

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,25 +1,40 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import css from "./Navigation.module.css";
 
 export const Navigation = () => {
+	const location = useLocation();
+
 	return (
 		<nav>
 			<ul className={css.nav}>
 				<li>
 					<NavLink className={css.item} to="/">
-						<img className={css.logo} src="/assets/icon-home.svg" />
-						<p className={css.description}>Home</p>
+						<img
+							className={`${css.logo} ${location.pathname === "/register" ? css["active-logo"] : ""}`}
+							src="/assets/icon-home.svg"
+							alt="Home"
+						/>
+
+						<p className={`${css.description} ${location.pathname === "/register" ? css.bold : ""}`}>Home</p>
 					</NavLink>
 				</li>
 				<li>
-					<NavLink className={css.item} to="#">
-						<img className={css.logo} src="/assets/icon-statistics.svg" />
-						<p className={css.description}>Statistics</p>
+					<NavLink className={css.item} to="/statistics">
+						<img
+							className={`${css.logo} ${location.pathname === "/statistics" ? css["active-logo"] : ""}`}
+							src="/assets/icon-statistics.svg"
+							alt="Statistics"
+						/>
+						<p className={`${css.description} ${location.pathname === "/statistics" ? css.bold : ""}`}>Statistics</p>
 					</NavLink>
 				</li>
 				<li>
-					<NavLink to="#">
-						<img className={`${css.logo} ${css.dolar}`} src="/assets/icon-dolar.svg" />
+					<NavLink className={`${css.item} ${css.dolar}`} to="#">
+						<img
+							className={`${css.logo} ${location.pathname === "#" ? css["active-logo"] : ""}`}
+							src="/assets/icon-dolar.svg"
+							alt="Dolar"
+						/>
 					</NavLink>
 				</li>
 			</ul>
