1ad7521b752010108808f64f3aa4b125e7634a02
Pawel-Eliszewski <eliszewskipawel@gmail.com>
2023-10-05 21:20:26 +0200

    Fixed Yup validation

--- src/utils/yupValidationSchema.js
+++ src/utils/yupValidationSchema.js
@@ -1,21 +1,18 @@
-import * as Yup from "Yup";
-export const registerValidationSchema = Yup.object().shape({
-  email: Yup.string()
-    .email("Invalid email address")
-    .required("Email is required"),
-  password: Yup.string()
+import { object, string, ref } from "yup";
+
+export const registerValidationSchema = object().shape({
+  email: string().email("Invalid email address").required("Email is required"),
+  password: string()
     .min(6, "Password must be at least 6 characters")
     .max(12, "Password must not exceed 12 characters")
     .required("Password is required"),
-  confirmPassword: Yup.string()
-    .oneOf([Yup.ref("password"), null], "Passwords must match")
+  confirmPassword: string()
+    .oneOf([ref("password"), null], "Passwords must match")
     .required("Confirm Password is required"),
 });
-export const loginValidationSchema = Yup.object().shape({
-  email: Yup.string()
-    .email("Invalid email address")
-    .required("Email is required"),
-  password: Yup.string()
+export const loginValidationSchema = object().shape({
+  email: string().email("Invalid email address").required("Email is required"),
+  password: string()
     .min(6, "Password must be at least 6 characters")
     .max(12, "Password must not exceed 12 characters")
     .required("Password is required"),
